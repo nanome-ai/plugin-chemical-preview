e8285e6ccd55ddb83026794c23e3270bc53b7c5b
Alex McNerney <ajm13@me.com>
2021-05-27 15:36:39 -0700

    Update integration tagging

--- nanome_chemical_preview/ChemicalPreview.py
+++ nanome_chemical_preview/ChemicalPreview.py
@@ -1,10 +1,10 @@
 import nanome
+from nanome.util.enums import Integrations
 
 from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
 
 import tempfile
-import shutil
 from cairosvg import svg2png
 
 # mol 2d image drawing options
@@ -54,7 +54,7 @@ class ChemicalPreview(nanome.PluginInstance):
         return image
 
 def main():
-    plugin = nanome.Plugin('2D Chemical Preview', 'A Nanome Plugin to generate 2D images of molecules using RDKit', 'Molecule Image', False)
+    plugin = nanome.Plugin('2D Chemical Preview', 'A Nanome Plugin to generate 2D images of molecules using RDKit', 'Molecule Image', False, integrations=[Integrations.generate_molecule_image])
     plugin.set_plugin_class(ChemicalPreview)
     plugin.run()
 
